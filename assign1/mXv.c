ff465924e05829d1c538e93219231033b1452e79
k213006-mohib-ahmed <162878682+k213006-mohib-ahmed@users.noreply.github.com>
2024-03-11 20:22:53 +0500

    dma and freeing memory function added by mohib mXv.c

--- assign1/mXv.c
+++ assign1/mXv.c
@@ -1,5 +1,5 @@
 /*
- * Programmer(s) : List all those who contributed in this file
+ * Programmer(s) : [k213006 mohib]
  * Date: 
  * Desc: Sequential version of matrix vector multiplication. 
  * Note: you will have to make similar files for other 4 version as explained in assign1.md and a batch file to get running times of all 5 programs.
@@ -7,6 +7,35 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//dynamic memory allocation function
+double **allocateMatrix(int rows,int cols)
+{
+  double ** matrix=(double **)malloc(rows* sizeof(double *));
+  if (matrix==NULL){
+    fprintf(stderr,"Memory allocation failed\n");
+    exit(EXIT_FAILURE);
+
+  }
+  for (int i = 0; i < rows; i++) {
+        matrix[i] = (double *)malloc(cols * sizeof(double));
+        if (matrix[i] == NULL) {
+            fprintf(stderr, "Memory allocation failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return matrix;
+
+
+}
+
+void freeMemory(double **matrix,int rows){
+  for (int i=0;i<rows;i++){
+    free(matrix[i]);
+
+  }
+  free(matrix)
+}
 
 int main(int argc, char* argv){
   //enter you code here...
