5d0670cde99eac323de0883c19a0eedb93274969
k213006-mohib-ahmed <162878682+k213006-mohib-ahmed@users.noreply.github.com>
2024-03-12 07:58:01 +0000

    changes done

--- assign1/mXv.c
+++ assign1/mXv.c
@@ -6,37 +6,122 @@
  */
 #include<stdio.h>
 #include<stdlib.h>
+#include<strings.h>
 
 //dynamic memory allocation function
-double **allocateMatrix(int rows,int cols)
+double **allocateMatrix(int size)
 {
-  double ** matrix=(double **)malloc(rows* sizeof(double *));
-  if (matrix==NULL){
-    fprintf(stderr,"Memory allocation failed\n");
-    exit(EXIT_FAILURE);
-
-  }
-  for (int i = 0; i < rows; i++) {
-        matrix[i] = (double *)malloc(cols * sizeof(double));
+  double **matrix = (double **)malloc(size * sizeof(double *));
+    if (matrix == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < size; i++) {
+        matrix[i] = (double *)malloc(size * sizeof(double));
         if (matrix[i] == NULL) {
             fprintf(stderr, "Memory allocation failed\n");
             exit(EXIT_FAILURE);
         }
     }
-
     return matrix;
 
 
 }
 
-void freeMemory(double **matrix,int rows){
-  for (int i=0;i<rows;i++){
+
+double *allocateVector(int size) {
+    double *vector = (double *)malloc(size * sizeof(double));
+    if (vector == NULL) {
+        fprintf(stderr, "Memory allocation of vector cannot be performed\n");
+        exit(EXIT_FAILURE);
+    }
+    return vector;
+}
+
+void freeMatrix(double **matrix,int size){
+  for (int i=0;i<size;i++){
     free(matrix[i]);
 
   }
-  free(matrix)
+  free(matrix);
 }
 
-int main(int argc, char* argv){
-  //enter you code here...
-}//end main
+void freeVector(double *vector) {
+    free(vector);
+}
+
+void fillMatrixRandom(double **matrix,int size){
+for (int i=0;i<size;i++){
+  for (int j=0;j<size;j++){
+    matrix[i][j]=((double) rand()/RAND_MAX)*1000;
+
+  }
+}
+
+}
+
+
+
+
+void fillVectorRandom(double *vector, int size) {
+    for (int i = 0; i < size; i++) {
+        vector[i] = ((double)rand() / RAND_MAX)*1000; 
+    }
+}
+
+double *matrixVectorMultiply(double **matrix, double *vector, int size) {
+    double *result = allocateVector(size);
+    for (int i = 0; i < size; i++) {
+        result[i] = 0.0;
+        for (int j = 0; j < size; j++) {
+            result[i] += matrix[i][j] * vector[j];
+        }
+    }
+    return result;
+}
+
+void displayVector(double *vector, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%f ", vector[i]);
+    }
+    printf("\n");
+}
+
+
+
+int main(int argc, char* argv[]){
+
+if (argc!=3){
+  fprintf(stderr,"Usage: %s <matrix_size>\n",argv[0]);
+  return EXIT_FAILURE;
+}
+
+
+int size=atoi(argv[1]); //get the size from cli
+if (size<=0){
+  fprintf(stderr,"Invalid size");
+  return EXIT_FAILURE;
+
+}
+
+double **matrix = allocateMatrix(size);
+double *vector = allocateVector(size);
+
+fillMatrixRandom(matrix, size);
+fillVectorRandom(vector, size);
+
+  double *result = matrixVectorMultiply(matrix, vector, size);
+
+    printf("Resultant vector:\n");
+    displayVector(result, size);
+
+    freeMatrix(matrix, size);
+    freeVector(vector);
+    freeVector(result);
+
+    return EXIT_SUCCESS;
+
+
+
+}
+
